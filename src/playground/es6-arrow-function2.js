dabf73abca8cea9f1d842e9fbc433e636e28acfe
Charlie Rogers <chubahay@gmail.com>
2021-04-22 06:13:13 +0100

    app split up into component files

--- src/playground/es6-arrow-function2.js
+++
@@ -1,9 +0,0 @@
-const multiplier = {
-    numbers: [2,4,6],
-    multiplyBy: 2,
-    multiply() {
-        return this.numbers.map((num) =>  num * this.multiplyBy)
-    }
-}
-
-console.log(multiplier.multiply());
