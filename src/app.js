4bd56dad857623d9a8bfae74bbdabfe874b895de
Charlie Rogers <chubahay@gmail.com>
2022-01-28 08:36:14 +0000

    changes made to expense item list and input

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux'
+import { addExpense, removeExpense, editExpense } from './actions/expenses'
+import { sortByAmount, sortByDate, setEndDate, setStartDate, setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
 
- 
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 200, createdAt: 2000 }));
+store.dispatch(setTextFilter('bill'));
+
+
+setTimeout(() => {
+store.dispatch(setTextFilter(''));
+}, 3000)
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
+
+
+console.log(store.getState())
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
+
