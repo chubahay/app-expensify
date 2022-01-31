0638a3ec195e3f4a94ee5b186080306298696e23
Charlie Rogers <chubahay@gmail.com>
2022-01-31 21:24:06 +0000

    more changes made

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,13 +6,19 @@ import 'react-dates/lib/css/_datepicker.css'
 const now = moment();
 //console.log(now.format('Do MMM YYYY'))
 export default class ExpenseForm extends React.Component {
-    state = {
-        description: '',
-        note: '',
-        amount: '',
-        createdAt: moment(),
-        calendarFocused: false,
-        error: ''
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            description: props.expense ? props.expense.description : '',
+            note: props.expense ? props.expense.note : '',
+            amount: props.expense ? (props.expense.amount / 100).toString() : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment() ,
+            calendarFocused: false,
+            error: ''
+    }
+    
+
     }
     onDescriptionChange = (e) => {
         const description = e.target.value
@@ -44,7 +50,12 @@ export default class ExpenseForm extends React.Component {
             this.setState(() => ({ error: 'Please provide a description and amount' }))
         } else {
             this.setState(() => ({ error: '' }))
-            console.log('Submmited')
+            this.props.onSubmit({
+                description: this.state.description,
+                amount: parseFloat(this.state.amount, 10) * 100,
+                createdAt: this.state.createdAt.valueOf(),
+                note: this.state.note
+            })
         }
     }
     render() {
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {removeExpense} from '../actions/expenses'
-import {connect} from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
+        <Link to={`/edit/${id}`}>
         <h3>{description}</h3>
-        <p>{amount} - {createdAt}</p>
-        <button onClick={() => {dispatch(removeExpense({ id }))}}>Remove</button>
+        </Link>
+        <p>{amount} - {createdAt}</p>     
     </div>    
 )
 
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
