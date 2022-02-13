8ed8ba5b462dec8cff9bbf06ebe6fb9eb4bc5c04
Charlie Rogers <chubahay@gmail.com>
2022-02-13 22:13:35 +0000

    updated expenselistitem component, and tests

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -7,7 +7,7 @@ export const ExpenseList = (props) => (
     <div>
     {
         props.expenses.length === 0 ? (
-            <p>THERE ARE NO EPXENSES</p>
+            <p>THERE ARE NO EXPENSES</p>
         ) : (
             props.expenses.map((expense) => {
             return <ExpenseListItem key={expense.id} {...expense} />;
